b8a96db235e990ea664f8f59f2ad7c3403d2d59f
FRA2721 <cimino.francesco36@gmail.com>
2022-10-06 15:56:32 +0200

    ticket train completed

--- js/script.js
+++ js/script.js
@@ -10,26 +10,31 @@ L'output del prezzo finale va messo fuori in forma umana (con massimo due decima
 
 // input-data
 const kmValue = parseFloat(prompt("How many km do you want to travel?"));
+
 const userAge = parseInt(prompt("How old are you?"));
 // ---------------------------------------------------------------------------------------
 
 // operations/conditions
-const infoPrice = " ";
+let infoPrice = ` `;
 const sentinelPrice = 0.21;
-const ticketPrice = kmValue*sentinelPrice;
+let ticketPrice = kmValue*sentinelPrice;
+ticketPrice = ticketPrice.toFixed(2);
 
-// discounts
+// discount 20%
 const ticketPrice20 = ticketPrice - ((ticketPrice/100)*20);
+
+// discount 40%
 const ticketPrice40 = ticketPrice - ((ticketPrice/100)*40);
 
+// check user age
 if (userAge < 18) { 
-    infoPrice = `You are under 18... You have a discount of 20%, so the price of your ticket is ${ticketPrice20}. Thank you for booking your ticket!`;
+    infoPrice = `You are under 18... You have a discount of 20%, so the price of your ticket is ${ticketPrice20}€. Thank you for booking your ticket!`;
 
 }else if (userAge > 65) {
-    infoPrice = `You are over 65... You have a discount of 40%, so the price of your ticket is ${ticketPrice40}. Thank you for booking your ticket!`;
+    infoPrice = `You are over 65... You have a discount of 40%, so the price of your ticket is ${ticketPrice40}€. Thank you for booking your ticket!`;
 
 }else {
-    infoPrice = `The price of your ticket is ${ticketPrice}. Thank you for booking your trip!`;
+    infoPrice = `The price of your ticket is ${ticketPrice}€. Thank you for booking your trip!`;
 }
 // ---------------------------------------------------------------------------------------
 
